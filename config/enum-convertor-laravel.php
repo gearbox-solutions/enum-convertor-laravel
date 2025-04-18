2c553f0519fddcb8a0270f6344213204624ad7d4
Michael Deck <michael@gearboxgo.com>
2025-04-18 17:55:37 -0400

    Update package defaults

---
+++ config/enum-convertor-laravel.php
@@ -0,0 +1,10 @@
+<?php
+
+// config for GearboxSolutions/EnumConvertor
+return [
+    'enum_paths' => [
+        'app/Enums' => 'resources/js/types/enums',
+    ],
+
+    'enum_extension' => '.d.ts',
+];
--- src/EnumConvertorServiceProvider.php
+++ src/EnumConvertorServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace GearboxSolutions\EnumConvertor;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use GearboxSolutions\EnumConvertor\Commands\EnumConvertorCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class EnumConvertorServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,8 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('enum-convertor-laravel')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_migration_table_name_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->hasCommand(EnumConvertorCommand::class);
     }
 }
